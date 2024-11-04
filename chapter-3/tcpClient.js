6a8763ec6d89098a4a1f3f077528ed09bf3ffb41
Du.Kang <banshee1115@163.com>
2024-11-04 15:50:18 +0800

    fix - 代码修改

--- chapter-3/tcpClient.js
+++ chapter-3/tcpClient.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Du.Kang banshee1115@163.com
+ * @Date: 2024-11-04 15:35:23
+ * @LastEditors: Du.Kang banshee1115@163.com
+ * @LastEditTime: 2024-11-04 15:38:33
+ * @FilePath: \node-learning\chapter-3\tcpClient.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 var net = require("net");
 
 var client = new net.Socket();
